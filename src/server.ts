818d04bbf0303a84511a21aef592f436a25d7b54
Leonardo Viana <leonardoviana00@hotmail.com>
2021-06-22 12:03:19 -0300

    user

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-/**
- * GET => Buscar uma informação
- * POST => Inserir uma informação
- * PUT => ALterar uma informação
- * DELETE => Remover um dado
- * PATCH => Alterar uma informação específica
- */
-
-//http://localhost:3000
-
-app.get("/teste", (request, response) => {
-    //Request => Entrando
-    //Response => Saindo
-
-    response.send("Olá, NLW");
-})
-
-app.post("/teste-post", (request, response) => {
-    response.send("Ola, NLW , Método post");
-})
 
 app.listen(3000, () => console.log("Server is running"));
