ca4261e6558c3211eb6241f61683d4e656faa2fc
Leonardo Viana <leonardoviana00@hotmail.com>
2021-06-24 11:51:43 -0300

    add compliment route

--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,19 @@ import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
 import { ensureAdmin  } from "./middlewares/ensuareAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CretaeComplimentController } from "./controllers/CreateComplimentController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authenticateUserController = new AuthenticateUserController();
+const createComplimentController = new CretaeComplimentController();
 
 router.post("/users", createUserController.handle);
 router.post("/tags", ensureAdmin, createTagController.handle);
+router.post("/login", authenticateUserController.handle);
+router.post("/compliments", createComplimentController.handle);
 
 export { router }
