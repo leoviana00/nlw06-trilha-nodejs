6a081ee719a54e1d895750605e6525d440d2de51
Leonardo Viana <leonardoviana00@hotmail.com>
2021-06-24 11:47:13 -0300

    ajuste do user services

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,15 +1,17 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from "../repositories/UsersRepositories";
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
     name: string;
     email: string;
     admin?: boolean;
+    password: string;
 }
 
 class CreateUserService {
 
-    async execute({ name, email, admin} : IUserRequest){
+    async execute({ name, email, admin = false, password} : IUserRequest){
         const usersRepository = getCustomRepository(UsersRepositories);
 
         //Verifica se o email está preenchido, caso não esteja o erro é lançado
@@ -27,11 +29,15 @@ class CreateUserService {
             throw new Error("User already exists");
         }
 
+        //Criptografando a senha do usuário
+        const passwordHash = await hash(password, 8)
+
         // S tiver tudo ok, uma nova instancia é criado é o objeto é salvo no banco de dados
         const user = usersRepository.create({
             name,
             email,
-            admin
+            admin,
+            password: passwordHash,
         })
 
         await usersRepository.save(user);
