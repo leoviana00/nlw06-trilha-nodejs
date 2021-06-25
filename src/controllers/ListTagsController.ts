62cab6db4186a5171853b59d43b1056b33302593
Leonardo Viana <leonardoviana00@hotmail.com>
2021-06-25 14:27:28 -0300

    List tags

--- src/controllers/ListTagsController.ts
+++ src/controllers/ListTagsController.ts
@@ -5,6 +5,7 @@ import { ListTagService } from "../services/ListTagsService";
 class ListTagController {
 
     async handle(request: Request, response: Response) {
+        
         const listTagService = new ListTagService();
 
         const tags = await listTagService.execute();
--- src/services/ListTagsService.ts
+++ src/services/ListTagsService.ts
@@ -1,11 +1,10 @@
 import { getCustomRepository } from "typeorm"
 import { TagsRepositories } from "../repositories/TagsRepositories"
 import { classToPlain } from "class-transformer";
-
-
-
 class ListTagService {
+
     async execute() {
+
         const tagsRepositories = getCustomRepository(TagsRepositories);
 
         const tags = await tagsRepositories.find();
